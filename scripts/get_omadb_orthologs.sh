770a9976c257d66d071674dc2e3ddf3d8d97f65d
Koen <24732704+kjradem@users.noreply.github.com>
2018-05-31 19:47:03 +0200

    Finalized function
    Selection of maximally 10 orthologs is included
    Data is given a header, results are written to the corresponding file.

--- scripts/get_omadb_orthologs.sh
+++ scripts/get_omadb_orthologs.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 while read id
 do
-  data=$(curl -s "https://omabrowser.org/api/protein/${id}/orthologs/")
-  id=">$id"
-  echo $id
-  
-  for row in $(echo "${sample}" | jq -r '.[] | @base64'); do
-	_jq() {
-		echo ${row} | base64 --decode | jq -r ${1}
-	}
-	
-	echo $(_jq '.canonicalid')
+  data=$(curl -s "https://omabrowser.org/api/protein/${id}/orthologs/" | jq -r '.[0:10]')
+  print_id=">$id"
+  echo $print_id >> $2
+
+  for row in $(echo "${data}" | jq -r '.[] | @base64'); do
+    _jq() {
+      echo ${row} | base64 --decode | jq -r ${1}
+    }
+    if [ -n "$(_jq '.canonicalid')" ]; then
+      echo $(_jq '.canonicalid') >> $2
+    fi
   done
 done
